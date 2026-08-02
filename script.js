4c2de81f0cd92152c0a1ac14885dd8a32f67519e
AbdulmalikY2026 <fideentv@gmail.com>
2026-08-03 00:28:58 +0100

    Rename registerMember to login and implement logic

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function registerMember(event) {
+function login(event) {
     event.preventDefault();
 
-    alert("🎉 Registration Successful!\n\nWelcome to FIDEEN.");
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+        localStorage.setItem("adminLogin", "true");
+        alert("✅ Login Successful!");
+        window.location.href = "admin.html";
+    } else {
+        alert("❌ Incorrect Username or Password");
+    }
+}
+
+function logout() {
+    localStorage.removeItem("adminLogin");
+    alert("Logged out successfully!");
+    window.location.href = "login.html";
 }
