4c07888e4d1fe38f1b39a57cfae2c0b5515ae0a9
AbdulmalikY2026 <fideentv@gmail.com>
2026-08-06 02:34:39 +0100

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyqBQaKQZ3mvRLimRa5mdVUkKDpOKifTI",
+  authDomain: "fideen-portal.firebaseapp.com",
+  projectId: "fideen-portal",
+  storageBucket: "fideen-portal.firebasestorage.app",
+  messagingSenderId: "984676075874",
+  appId: "1:984676075874:web:5f703e73f19cd8ebb4c43b",
+  measurementId: "G-EHTME0T3R1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
